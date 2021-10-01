ebe170e63260e2054c6a996318ffa8a482081e6d
Jenny Lin <misslinyang91@gmail.com>
2021-10-01 19:16:57 +1000

    created user card and button component

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button/Button";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Uername</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
